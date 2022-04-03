f0d1d99eb2ca489482381f3babfc14d5240cc1a1
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-04-03 06:41:49 +0100

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.userLogin);
+  React.useEffect(() => {
+    if (userInfo) {
+      navigate("/login");
+    } else {
+      navigate("/products");
+    }
+  }, [navigate, userInfo]);
 };
 
 export default Home;
